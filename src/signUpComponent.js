cfec78296c0d482b9b8e8eec854024fd5cfe824a
Jawadban <jawadbanga@gmail.com>
2017-02-28 23:55:52 -0800

    implementing the auth

--- src/signUpComponent.js
+++ src/signUpComponent.js
@@ -15,9 +15,9 @@ class SignUp extends React.Component {
   }
 
   handleChange(event) {
-    //this.setState({value: event.target.value})
+    //this.setState({value: event.target.value});
     const target = event.target;
-    //const value = target.type === 'text' ? target.checked : target.value;
+    //const value = target.type === 'text' ? target.checked : target.value
     const name = target.name;
 
     this.setState({
