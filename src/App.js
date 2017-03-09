d1fe14cd3b2e5de6e21fcef8ba4e58efe627f55c
Jawadban <jawadbanga@gmail.com>
2017-03-08 23:55:13 -0800

    have to add facebook post feature

--- src/App.js
+++ src/App.js
@@ -254,7 +254,7 @@ class App extends React.Component {
 //   var userId = firebase.auth().currentUser.uid;
 // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
 //   var username = snapshot.val().username;
-//   // 
+//   /
 // });
 
   componentDidMount() {
