ac25c3d29a62e82e44cc5015b9451014ffdf78d2
Jawadban <jawadbanga@gmail.com>
2017-03-14 23:54:07 -0700

    adding Facebook Graph API usage to post on user feed

---
+++ src/FacebookApiUse.js
@@ -0,0 +1,9 @@
+    // FB.setAccessToken('access_token');
+    // var body = 'My first post using facebook-node-sdk';
+    // FB.api('me/feed', 'post', { message: body }, function (res) {
+    //   if(!res || res.error) {
+    //     console.log(!res ? 'error occurred' : res.error);
+    //     return;
+    //   }
+    //   console.log('Post Id: ' + res.id);
+    // });
